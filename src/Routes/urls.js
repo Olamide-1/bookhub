68b63c07b6c3fb6bd3fea0ef92d0bb353ce63011
Olamide-1 <abimbolaolamidee@gmail.com>
2023-04-12 12:18:34 -0400

    Changed google console client id

--- src/Routes/urls.js
+++ src/Routes/urls.js
@@ -1,6 +1,9 @@
 /** BASE URL FOR BACKEND API */
 const baseURL = "http://booksoreapi-env.eba-3igtf73b.us-east-1.elasticbeanstalk.com";
 
+/** WEB App url */
+const webURL = "http://bookstore-webapp.s3-website-us-east-1.amazonaws.com";
+
 const PATHS = {
     register: "/account/register/",
     login: "/account/login/",
@@ -13,11 +16,11 @@ const PATHS = {
     walletOAUTH: "/oauth/wallet-payment/",
 };
 
-const clientID = "776429688428-v6j8mk4ei83nkmgi6vvpbcfo2ma8r47j.apps.googleusercontent.com";
+const clientID = "13313401862-k6jo5a8i07ajp2pnjso2uehbl9l280lk.apps.googleusercontent.com";
 
 const walletURL = "http://wallet-env.eba-gr5bgv3s.eu-north-1.elasticbeanstalk.com";
 const walletClientID = "xRTdyvaizhdE4Az1IqTOrTCYbXbgZGCsJUWrtV7A";
 const walletCodeChallange = "7pv0xJir-5nl3uAnh4-stnIWlymb6MAQ6-cypRPkG8M";
-const redirectURI = "http://localhost:3000/oauth/wallet";
+const redirectURI = `${webURL}/oauth/wallet`;
 
 export { baseURL, PATHS, clientID, walletURL, walletClientID, walletCodeChallange, redirectURI };
